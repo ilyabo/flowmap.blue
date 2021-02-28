7acd633d4f1fbe10f7156e24be4e478447b4d8f3
Ilya Boyandin <ilyabo@gmail.com>
2021-02-28 01:05:18 +0100

    Moved typings

--- typings/typings.d.ts
+++ typings/typings.d.ts
@@ -1,1 +1,10 @@
 declare module 'kdbush';
+declare module '@deck.gl/core';
+declare module '@deck.gl/react';
+declare module '@deck.gl/layers';
+declare module '@luma.gl/core';
+declare module '@luma.gl/constants';
+declare module 'kdbush';
+declare module 'react-map-gl';
+declare module 'react-map-gl-draw';
+declare module 'mjolnir.js';
